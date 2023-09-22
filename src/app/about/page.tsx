9733d6cce090ce30d90c47ea0fd7adedd830c877
Andrey Kulyk <>
2023-09-22 15:00:28 +0300

    Start of the WhatIs block.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import { WhatIs } from "./components/WhatIs";
+
+export default function About() {
+    return (
+      <div className="bg-blue-900 p-32">
+        <WhatIs />
+      </div>
+    );
+  }
+  
