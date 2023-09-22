6d8f5f9cd5bfa858c592e7e792c696c133da238f
Andrey Kulyk <>
2023-09-22 19:20:07 +0300

    Fonts added + added to the global css

--- src/app/about/components/WhatIs.tsx
+++ src/app/about/components/WhatIs.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import imagePath from "../../../../public/images/what-is-1.png"
+import "../../../app/globals.css";
 import { ABOUT_WHATIS__URL } from "../../URL's";
 
 export function WhatIs() {
   return (
     <div>
       <div className="flex justify-between items-center mb-10">
-        <div className="text-5xl font-bold text-white border-b-4 border-blue-500 inline p-2">WHAT IS SPORTSBOX AI?</div>
+        <div className="anton-font text-5xl font-bold text-white border-b-4 border-blue-500 inline p-2">WHAT IS SPORTSBOX AI?</div>
         <div className="text-white w-[45%]">
           Founded in 2020, Sportsbox AI is an AI-powered technology company that develops AI Coach mobile applications
           in sports and fitness, using patent-pending 3D Motion Analysis and Kinematic AI technology.
