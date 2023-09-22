167a38a965834b177b0a5359673f66405537d1d6
nikitad2 <nikitad@sportsbox.ai>
2023-09-22 15:06:05 +0300

    fix what is

--- src/app/about/components/WhatIs.tsx
+++ src/app/about/components/WhatIs.tsx
@@ -1,14 +1,23 @@
-export function WhatIs(){
-return <div>
-    <div className="flex">
-        <div className="text-3xl font-bold text-white border-b-2 border-blue-500 inline flex-1">What is SportsBox AI?</div>
-        <div className="text-white flex-1">Founded in 2020, Sportsbox AI is an AI-powered technology company that develops AI Coach mobile applications in sports and fitness, using patent-pending 3D Motion Analysis and Kinematic AI technology.</div>
-    </div>
-
+export function WhatIs() {
+  return (
     <div>
-        <div>Through our unique technology that can capture, measure and analyze complex athletic motions in 3D, combined with the knowledge and experience of the world’s leading instructors in each sport, Sportsbox AI provides real-time corrective feedback to help users achieve their goals in sports and fitness — all in the palm of your hand using only your mobile phone and at a fraction of the cost of existing 3D Motion Capture Systems.</div>
+      <div className="flex justify-between items-center">
+        <div className="text-3xl font-bold text-white border-b-2 border-blue-500 inline">What is SportsBox AI?</div>
+        <div className="text-white w-[50%]">
+          Founded in 2020, Sportsbox AI is an AI-powered technology company that develops AI Coach mobile applications
+          in sports and fitness, using patent-pending 3D Motion Analysis and Kinematic AI technology.
+        </div>
+      </div>
+
+      <div>
+        <div>
+          Through our unique technology that can capture, measure and analyze complex athletic motions in 3D, combined
+          with the knowledge and experience of the world’s leading instructors in each sport, Sportsbox AI provides
+          real-time corrective feedback to help users achieve their goals in sports and fitness — all in the palm of
+          your hand using only your mobile phone and at a fraction of the cost of existing 3D Motion Capture Systems.
+        </div>
         <div>Image</div>
+      </div>
     </div>
-    
-</div>
+  );
 }
