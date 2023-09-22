c9a387875bbb4690a32dcb80afb81259b927eba7
Andrey Kulyk <>
2023-09-22 15:50:54 +0300

    Changed the WhatIs block

--- src/app/about/components/WhatIs.tsx
+++ src/app/about/components/WhatIs.tsx
@@ -1,22 +1,24 @@
+import Image from "next/image";
+import imagePath from "../../../../public/images/what-is-1.png"
+
 export function WhatIs() {
   return (
     <div>
-      <div className="flex justify-between items-center">
-        <div className="text-3xl font-bold text-white border-b-2 border-blue-500 inline">What is SportsBox AI?</div>
-        <div className="text-white w-[50%]">
+      <div className="flex justify-between items-center mb-10">
+        <div className="text-5xl font-bold text-white border-b-4 border-blue-500 inline p-2">WHAT IS SPORTSBOX AI?</div>
+        <div className="text-white w-[45%]">
           Founded in 2020, Sportsbox AI is an AI-powered technology company that develops AI Coach mobile applications
           in sports and fitness, using patent-pending 3D Motion Analysis and Kinematic AI technology.
         </div>
       </div>
 
-      <div>
-        <div>
+      <div className="flex justify-between items-center mb-10">
+        <div className="text-white w-[45%]">
           Through our unique technology that can capture, measure and analyze complex athletic motions in 3D, combined
           with the knowledge and experience of the world’s leading instructors in each sport, Sportsbox AI provides
           real-time corrective feedback to help users achieve their goals in sports and fitness — all in the palm of
           your hand using only your mobile phone and at a fraction of the cost of existing 3D Motion Capture Systems.
         </div>
-        <div>Image</div>
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-"use client";
-
-import { Form } from "../components/Form";
 
 export default function Home() {
   return (
     <div>
-      <Form input1="blue" input2="red" />
+      
     </div>
   );
 }
