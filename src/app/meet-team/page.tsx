f04341a2482413a4d4bedb1cd44a32f06422b9ff
Andrey Kulyk <>
2023-11-10 11:51:04 +0200

    Added separator to the meet the team

--- src/app/meet-team/page.tsx
+++ src/app/meet-team/page.tsx
@@ -2,10 +2,12 @@ import { FoundingTeam } from "./components/FoundingTeam";
 import { Advisors } from "./components/Advisors";
 import { GolfAdvisors } from "./components/GolfAdvisors";
 import { SportsScienceAdvisors } from "./components/SportsScienceAdvisors";
+import { Separator } from "@/components/Separator";
 
 export default function MeetTeam() {
   return (
     <div className="flex flex-col items-center gap-y-24 p-8">
+      <Separator height={200} />
       <FoundingTeam />
       <Advisors />
       <GolfAdvisors />
