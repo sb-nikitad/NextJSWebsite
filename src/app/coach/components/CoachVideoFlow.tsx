ba4c1cf59c29d8c01e6811e3b4e6f39f5985cd74
Andrey Kulyk <>
2023-11-27 19:21:05 +0200

    Coach page - done.

--- src/app/coach/components/CoachVideoFlow.tsx
+++ src/app/coach/components/CoachVideoFlow.tsx
@@ -62,6 +62,43 @@ export const CoachVideoFlow = () => {
           </div>
         </div>
       </div>
+
+      <div className="bg-sb-darkblue flex justify-start w-full px-[180px] py-[20px] gap-x-4">
+        <div className="flex flex-col gap-4 w-2/3 justify-center">
+          <div className={`${anton.className} text-6xl whitespace-pre-wrap`}>
+            {"IMPORT AND ANALYZE VIDEOS FROM YOUR INBOX"}
+          </div>
+          <div className="text-[18px] font-light">
+            Track your students’ progress and message them directly through the
+            app
+          </div>
+        </div>
+        <div className="">
+          <video autoPlay muted loop className="w-[300px]">
+            <source src="videos/coach/Coach_4.mp4" type="video/mp4" />
+          </video>
+        </div>
+      </div>
+
+      <div className="bg-sb-darkblue flex flex-col justify-center items-center w-full px-[180px] py-[20px] gap-y-12 pb-16">
+        <div className="flex flex-col gap-y-4 w-fit justify-center items-center">
+          <div
+            className={`${anton.className} text-6xl whitespace-pre-wrap text-center	`}
+          >
+            {"COMPARE TWO SWINGS WITH\n 3D MOTION DATA"}
+          </div>
+          <div className="text-[18px] font-light whitespace-pre-wrap text-center	`">
+            {
+              "Quantify the difference between before and after\n swings or use a pro’s swing as a model reference"
+            }
+          </div>
+        </div>
+        <div className="flex">
+          <video autoPlay muted loop className="w-[750px]">
+            <source src="videos/coach/Coach_6.mp4" type="video/mp4" />
+          </video>
+        </div>
+      </div>
     </>
   );
 };
--- src/app/coach/page.tsx
+++ src/app/coach/page.tsx
@@ -2,26 +2,14 @@ import { HoverButton } from "../../components/HoverButton/HoverButton";
 import clsx from "clsx";
 import { CoachThumbnail } from "./components/CoachThumbnail";
 import { CoachVideoFlow } from "./components/CoachVideoFlow";
-import { MainCoachPlayerRedirect } from ".././components/MainCoachPlayerRedirect";
-import { WhatIs } from ".././about/components/WhatIs";
-import { Separator } from "../../components/Separator";
-import { Technology } from ".././about/components/Technology";
-import { MainTeam } from ".././components/MainTeam";
-import { MainTestimonialsCarousel } from ".././components/MainTestimonialsCarousel";
+import { CoachStarted } from "./components/CoachStarted";
 
 export default function HomePage() {
   return (
     <div className="">
       <CoachThumbnail />
       <CoachVideoFlow />
-      <MainCoachPlayerRedirect />
-      <div className="px-[250px]">
-        <WhatIs />
-      </div>
-      <Separator height={100} />
-      <Technology />
-      <MainTeam />
-      {/* <MainTestimonialsCarousel /> */}
+      <CoachStarted />
     </div>
   );
 }
