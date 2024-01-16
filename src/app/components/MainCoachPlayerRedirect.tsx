dab4340777e0b6e900020e3b923df1c289ddaaca
nikitad2 <nikitad@sportsbox.ai>
2024-01-16 15:53:08 +0200

    fix

--- src/app/components/MainCoachPlayerRedirect.tsx
+++ src/app/components/MainCoachPlayerRedirect.tsx
@@ -2,7 +2,7 @@ import { HoverButton } from "../../components/HoverButton/HoverButton";
 
 export const MainCoachPlayerRedirect = () => {
   return (
-    <div className="px-10 lg:px-[250px] flex justify-between py-[100px] flex-wrap">
+    <div className="px-10 lg:px[100pxg] 2xl:px-[250px] flex justify-between py-[100px] flex-wrap">
       <div className="bg-[url('/images/main/for-players.jpeg')] bg-cover bg-center w-full lg:w-1/2 lg:h-[600px] p-[150px] flex items-end justify-center">
         <HoverButton
           color="bg-sb-blue"
