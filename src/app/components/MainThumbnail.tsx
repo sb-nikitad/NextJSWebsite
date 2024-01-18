f52b86bad1dd7fe0b33f794bc384b77338505f46
nikitad2 <nikitad@sportsbox.ai>
2024-01-18 16:09:37 +0200

    fix

--- src/app/components/MainThumbnail.tsx
+++ src/app/components/MainThumbnail.tsx
@@ -5,8 +5,8 @@ const anton = Anton({ weight: "400", subsets: ["latin"] });
 export const MainThumbnail = () => {
   return (
     <>
-      <div className="bg-[url('/images/main-bg.jpg')] bg-cover bg-center flex justify-between w-full xl:px-[250px] xl:py-[200px] py-[100px] flex-wrap ">
-        <div className="flex flex-col gap-10 p-5 md:w-full xl:w-fit">
+      <div className="bg-[url('/images/main-bg.jpg')] bg-cover bg-center flex justify-between items-center w-full md:px-[50px] xl:px-[100px] xl:py-[200px] py-[100px] flex-wrap ">
+        <div className="flex flex-col gap-10 p-5 sm:w-full md:w-fit">
           <div className={`${anton.className} text-6xl whitespace-pre-wrap `}>
             {"3D MOTION CAPTURE\nIN THE PALM\nOF YOUR HANDS"}
           </div>
@@ -25,12 +25,12 @@ export const MainThumbnail = () => {
             />
           </div>
         </div>
-        <div className="flex items-end justify-center mt-10 xl:mt-0 sm:w-[300px] md:w-full xl:w-fit">
+        <div className="flex items-end justify-center mt-10 xl:mt-0 sm:w-[300px] md:w-fit xl:w-[400px] 2xl:w-[500px]">
           <video
             autoPlay
             muted
             loop
-            className="w-[150px] md:w-[200px] relative top-0 left-10 xl:left-20 rounded-[35px] z-20"
+            className="w-[150px] md:w-[180px] relative top-0 left-10 xl:left-20 rounded-[35px] z-20"
           >
             <source src="videos/main-1.mp4" type="video/mp4" />
           </video>
@@ -38,7 +38,7 @@ export const MainThumbnail = () => {
             autoPlay
             muted
             loop
-            className="w-[290px] md:w-[400px] xl:w-[400px] rounded-[20px] xl:rounded-[35px] -left-20 relative z-0"
+            className="w-[290px] md:w-[350px] xl:w-[400px] rounded-[20px] xl:rounded-[35px] -left-20 relative z-0"
           >
             <source src="videos/main-2.mp4" type="video/mp4" />
           </video>
