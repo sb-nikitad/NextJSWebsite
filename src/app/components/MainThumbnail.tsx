b0cc2a5f42ee84f2761032315bea2a3935f8da2a
Andrey Kulyk <>
2024-01-19 12:07:31 +0200

    https://sportsboxai.atlassian.net/browse/WEB-1848 - fixed for ipad

--- src/app/components/MainThumbnail.tsx
+++ src/app/components/MainThumbnail.tsx
@@ -5,12 +5,14 @@ const anton = Anton({ weight: "400", subsets: ["latin"] });
 export const MainThumbnail = () => {
   return (
     <>
-      <div className="bg-[url('/images/main-bg.jpg')] bg-cover bg-center flex justify-between items-center w-full md:px-[50px] xl:px-[100px] xl:py-[200px] py-[100px] flex-wrap ">
+      <div className="bg-[url('/images/main-bg.jpg')] bg-cover bg-center flex justify-between items-center w-full md:px-[50px] xl:px-[100px] xl:py-[200px] py-[100px] flex-wrap md:flex-nowrap">
         <div className="flex flex-col gap-10 p-5 sm:w-full md:w-fit">
-          <div className={`${anton.className} text-6xl whitespace-pre-wrap `}>
+          <div
+            className={`${anton.className} text-6xl whitespace-pre-wrap md:text-5xl`}
+          >
             {"3D MOTION CAPTURE\nIN THE PALM\nOF YOUR HANDS"}
           </div>
-          <div className="flex justify-center xl:justify-between flex-wrap gap-4 xl:w-[500px]">
+          <div className="flex justify-center xl:justify-between md:justify-start flex-wrap gap-4 xl:w-[500px]">
             <HoverButton
               className="w-full xl:w-52"
               color="bg-sb-blue"
@@ -30,7 +32,7 @@ export const MainThumbnail = () => {
             autoPlay
             muted
             loop
-            className="w-[150px] md:w-[180px] relative top-0 left-10 xl:left-20 rounded-[35px] z-20"
+            className="w-[150px] md:w-[160px] relative top-0 left-10 md:left-10 xl:left-20 rounded-[35px] z-20"
           >
             <source src="videos/main-1.mp4" type="video/mp4" />
           </video>
@@ -38,7 +40,7 @@ export const MainThumbnail = () => {
             autoPlay
             muted
             loop
-            className="w-[290px] md:w-[350px] xl:w-[400px] rounded-[20px] xl:rounded-[35px] -left-20 relative z-0"
+            className="w-[290px] md:w-[300px] xl:w-[400px] rounded-[20px] xl:rounded-[35px] -left-20 md:-left-10 relative z-0"
           >
             <source src="videos/main-2.mp4" type="video/mp4" />
           </video>
